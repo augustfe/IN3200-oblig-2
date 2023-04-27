754f8c2de6c6b89f1d587af120f199a0949f4e37
August Femtehjell <augustfe@math.uio.no>
2023-04-27 11:42:21 +0200

    Fixed computed numerical error

--- mpi_part/all_compute_numerical_error.c
+++ mpi_part/all_compute_numerical_error.c
@@ -1,11 +1,12 @@
 #include <math.h>
+#include <mpi.h>
 #include <stdio.h>
 #define Pi M_PI
 
 double all_compute_numerical_error(int my_rank, int my_offset, int P, int nx, int my_ny, double dx, double dy,
                                    double t_value, double **my_u)
 {
-    double S = 0, tmp;
+    double local_S = 0, tmp, S;
     int i, j, has_neigh_below;
 
     has_neigh_below = (my_rank > 0) ? 1 : 0;
@@ -15,8 +16,10 @@ double all_compute_numerical_error(int my_rank, int my_offset, int P, int nx, in
         {
             tmp = cos(2.0 * Pi * (i + my_offset) * dy) * cos(2.0 * Pi * j * dx) * cos(Pi * t_value) -
                   my_u[i + has_neigh_below][j];
-            S += tmp * tmp;
+            local_S += tmp * tmp;
         }
+
+    MPI_Allreduce(&local_S, &S, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
     return sqrt(dx * dy * S);
 }
